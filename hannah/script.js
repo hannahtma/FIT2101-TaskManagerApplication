93a2ca3a8a2c458ff3eed2544300f30c59ac0058
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-09-09 22:16:14 +0800

    Updated script.js
    Save items to local storage

--- hannah/script.js
+++ hannah/script.js
@@ -1,76 +1,144 @@
-localStorage;
 
-// Showing the Task
-const showCard = document.getElementById('show-card');
-const showTask = document.getElementById('show-task');
+const TASK_KEY = "allTasksData";
+const allTasks = [];
 
-// Display enlarged Task with all details of the card with "Edit Task" button
-const popupContainer = document.getElementById('popup-container');
-
-showTask.addEventListener('click', () => {
-    popupContainer.classList.add('show');
-});
-
-// Popup window that prompts for user to enter new task details
-const editCard = document.getElementById('edit-card');
-const editContainer = document.getElementById('edit-container');
-
-editCard.addEventListener('click', () => {
-    editContainer.classList.add('show');
-});
-
-// When "Cancel" button is clicked, popup disappears and goes back to original card layout
-const cancelEdit = document.getElementById('cancel-edit');
-
-cancelEdit.addEventListener('click', () => {
-    editContainer.remove('show');
-});
-
-// When "Save and Exit" button is clicked, new task details will be replaced
-//const saveAndExit = document.getElementById('save-and-exit');
-//const taskHeader = document.getElementById('task-header');
-//const taskContent = document.getElementById('task-content');
-
-function updateCard(){
-    let taskName = document.getElementById('task-name').value;
-    let taskDesc = document.getElementById('task-desc').value;
-    displayCard(taskName, taskDesc);
+class Task {
+    constructor(taskList = []) {
+        this._taskList = taskList;
+        }
+    //-------ACCESSORS---------------
+    get taskList() {return this._taskList};
+    //-------MUTATORS-----------------
+    set taskList(newTaskList) {this._taskList = newTaskList};
+    //-------METHOD---------------
+    fromData(data) {
+        this._taskList = data._taskList;
+    }
 }
 
-function displayCard(taskName, taskDesc){
-    let getIdOfContainer = document.getElementById('task-container');
-    let displayData = `<div class="show-task" id="show-task">
-                            <h1 id="task-header">${taskName}</h1>
-                            <p id="task-content">${taskDesc}</p>
-                            <div class="dropdown">
-                                <div class="select">
-                                    <span class="selected">Tags</span>
-                                    <div class="caret"></div>
-                                </div>
-                                <u1 class="menu">
-                                    <li>Core</li>
-                                    <li>UI</li>
-                                    <li>All</li>
-                                </u1>
-                            </div>
-                        </div>`;
+function addTaskCard() {
+    // TODO: Task 4
+    //Collecting Data from ID's
+    let newTitle = document.getElementById("task-title").value;
+    let newDesc = document.getElementById("task-desc").value;
 
-    return (getIdOfContainer.innerHTML = displayData);
+    // Try again if empty input
+    while (newTitle == "" || newDesc == "") {
+        alert("That input is invalid");
+        if (newTitle == null || newTitle == "") {
+            newTitle = prompt("Name of Title?");
+        } else if (newDesc == null || newDesc == "") {
+            newDesc = prompt("Description?");
+        }
+    }
+
+    console.log(allTasks);
+    let someTask = {"taskName": newTitle, "taskDesc": newDesc};
+    allTasks.push(someTask);
+    data = new Task(allTasks);
+    let json = JSON.stringify(data);
+    localStorage.setItem(TASK_KEY, json);
+
+    //Updating the display table/inventory with the user entered data
+    // update LS
+    // updateLSData(WAREHOUSE_KEY, inventory);
+    // update display
+    displayCard(newTitle, newDesc);
+    $("#exampleModal").modal("hide");
+    $("#exampleModal").on("hidden.bs.modal", function (e) {
+        $("#exampleModal").find("input[type=text], textarea").val("");
+    });
+
 }
 
-//saveAndExit.addEventListener('click', function() {
-    //editContainer.remove('show');
-    //popupContainer.classList.remove('show');
-    //localStorage.setItem('task-name', taskName);
-    //localStorage.setItem('task-desc', taskDesc);
-    //let taskName = document.getElementById('task-name').value;
-    //let taskDesc = document.getElementById('task-desc').value;
-    //console.log(taskName);
-    //taskHeader.textContent = taskName;
-    //taskContent.textContent = taskDesc;
-    let displayCard = `<div class="show-task" id="show-task">
-                        <h1 id="task-header">${taskName}</h1>
-                        <p id="task-content">${taskDesc}</p>
+function displayCard(title, desc) {
+    // TODO: Task 3
+    let displayRef = document.getElementById("my-container");
+    let displayData = "";
+    // This is used to display the entered data and add an edit button next to it
+    displayData += `<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text" id="studentEnrolSummary">${desc}</p>
+                    <a href="#show-task-container" class="card-link stretched-link">Card link</a>
+                </div>
+                </div>`;
+
+    return (displayRef.innerHTML = displayData);
+}
+
+function edit(taskName, taskDesc) {
+    // store data in LS
+    localStorage.setItem(TASK_NAME_KEY, taskName);
+    localStorage.setItem(TASK_DESC_KEY, taskDesc);
+    // redirect to edit page
+    window.location = "#show-task-container";
+}
+
+/**
+ * checkLSData function
+ * Used to check if any data in LS exists at a specific key
+ * @param {string} key LS Key to be used
+ * @returns true or false representing if data exists at key in LS
+ */
+function checkLSData(key) {
+    if (localStorage.getItem(key) != null) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * retrieveLSData function
+ * Used to retrieve data from LS at a specific key.
+ * @param {string} key LS Key to be used
+ * @returns data from LS in JS format
+ */
+function retrieveLSData(key) {
+    let data = localStorage.getItem(key);
+    try {
+        data = JSON.parse(data);
+    } catch (err) {
+    } finally {
+        return data;
+    }
+}
+
+/**
+ * updateLSData function
+ * Used to store JS data in LS at a specific key
+ * @param {string} key LS key to be used
+ * @param {any} data data to be stored
+ */
+function updateLSData(key, data) {
+    let json = JSON.stringify(data);
+    localStorage.setItem(key, json);
+}
+
+function changeColor(getColor){
+    let color = document.querySelector('.color');
+    let selectColor = getColor.value;
+    color.style.background = selectColor;
+}
+
+function saveAndExitTaskPopup() {
+    closeTaskPopup();
+    closeFullTaskPopup();
+
+    let taskName = document.getElementById("task-name").value;
+    let taskDesc = document.getElementById("task-desc").value;
+
+    updateTask();
+}
+
+function updateTask(taskName, taskDesc) {
+    let displayTask = document.getElementById("edit-container");
+    let displayData = `<div class="edit-container" id="edit-container">
+                    <div class="edit">
+                        <input id="task-name"><br>
+                        <input id="task-desc"><br>
+                        <button class="cancel-edit" id="cancel-edit" onclick="location.href='#popup-container'">Cancel</button>
+                        <button class="save-and-exit" id="save-and-exit" onclick="updateCard()">Save & Exit</button>
                         <div class="dropdown">
                             <div class="select">
                                 <span class="selected">Tags</span>
@@ -82,38 +150,8 @@ function displayCard(taskName, taskDesc){
                                 <li>All</li>
                             </u1>
                         </div>
-                    </div>`
+                    </div>
+                </div>`;
 
-    //let getIdOfContainer = document.getElementById('task-container');
-    //getIdOfContainer.innerHTML = displayCard;
-//});
-
-// Created a drop down menu that allows user to tag the task
-const dropdowns = document.querySelectorAll('.dropdown');
-
-dropdowns.forEach(dropdown => {
-    const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.menu');
-    const options = dropdown. querySelectorAll('.menu li')
-    const selected = dropdown.querySelector('.selected');
-
-    select.addEventListener('click', () => {
-        select.classList.toggle('select-clicked');
-        caret.classList.toggle('caret-rotate');
-        menu.classList.toggle('menu-open');
-    });
-
-    options.forEach(option => {
-        option.addEventListener('click', () => {
-            selected.innerText = option.innerText;
-            select.classList.remove('select-clicked');
-            caret.classList.remove('caret-rotate');
-            menu.classList.remove('menu-open');
-            options.forEach(option => {
-                option.classList.remove('active');
-            });
-            option.classList.add('active');
-        });
-    });  
-});
+    return (displayTask.innerHTML = displayData);
+}
