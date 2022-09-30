17acc58ac51e44bf61f178df92c096c35a405b8a
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-09-30 18:34:21 +0800

    Created SprintBoard.vue
    Created a SprintBoard window

---
+++ Vue/src/views/SprintBoard.vue
@@ -0,0 +1,24 @@
+<template>
+    <body>
+        <AddSprintBoard :sprints="this.sprints" @display-sprint-board="addSprintBoards"/>
+        <h2>My Sprints</h2>
+    </body>
+</template>
+
+<script>
+    import Button from '@/components/Button.vue';
+    import AddSprintBoard from '@/components/addSprintBoard.vue';
+
+    import { ref } from 'vue';
+
+    export default {
+        components: {
+            Button,
+            AddSprintBoard,
+        },
+
+        methods: {
+
+        }
+    }
+</script>
