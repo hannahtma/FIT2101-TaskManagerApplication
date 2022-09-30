16e033a1ab8735cc099a7f8f603354947a487bfe
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-09-30 17:42:54 +0800

    Created ProductBacklog.vue
    Coded js for product backlog

---
+++ Vue/src/views/ProductBacklog.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        props: {
+            cards: Array,
+        },
+
+        mounted() {
+            this.productBacklogCards = this.cards
+        },
+
+        methods: {
+            getCardsForProductBacklog() {
+                return this.cards;
+            },
+        },
+
+        data() {
+            return {
+                productBacklogCards: [],
+            }
+        }
+    }
+</script>
