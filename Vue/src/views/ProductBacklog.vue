ffc0fdb9392ed70247c8544c009eee983767aa3b
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-10-01 22:36:28 +0800

    Updated ProductBacklog.vue
    Deleted some code that wasn't necessary

--- Vue/src/views/ProductBacklog.vue
+++ Vue/src/views/ProductBacklog.vue
@@ -1,49 +1,23 @@
 <template>
     <body>
-        <nav class="navbar navbar-expand-sm">
-            <!-- Brand -->
-            <a class="navbar-brand" href="index.html">SCRUMFY</a>
-            <!-- Links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" id="navLink" href="#">Dashboard</a>
-                </li>
-                <li class="nav-item dropdown" style="position:relative; left:30px;">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">Sprints</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
-                    </div>
-                </li>
-                <!-- Dropdown -->
-                <li class="nav-item dropdown" style="position:relative; left:60px;">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">Tasks</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown" style="position:relative; left:90px;">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown3" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">Teams</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
+        <AddCardToProductBacklog :cards="this.cards" @display-card-in-product-backlog="addTaskCardToProductBacklog"/>
         <div class="app">
             <div class="lists">
                 <div class="list">
-                    <div class="list-item" draggable="true">User Story 1</div>
-                    <div class="list-item" draggable="true">User Story 2</div>
-                    <div class="list-item" draggable="true">User Story 3</div>
+                    <div class="list">
+                        <!-- <div class="row" id="progress3">Deployed</div> -->
+                    <div @click="displayProductBacklog()" class="product backlog row" :class="card.id" v-for="card in this.displayCards">
+                        <h3>{{card.taskName}}</h3>
+                        <p>Description: {{card.description}}</p>
+                        <div>Status: {{card.status}}</div>
+                        <div>Type: {{card.type}}</div>
+                        <div class="story-points">
+                            {{card.storyPoints}}
+                        </div>
+                        <div>Assigned To: {{card.assign}}</div>
+                        <span class="tag" v-for="tag in card.tags">{{tag}}</span>
+                    </div>
+                    </div>
                 </div>
                 <div class="list"></div>
             </div>
@@ -52,68 +26,33 @@
 </template>    
 
 <script>
-    function addSprint() {
-        let title = document.getElementById("title-name").value;
-        var button = document.createElement("button");
-        button.innerHTML = title;
-        button.type = "button";
-        button.className = "btn btn-primary";
-        button.onclick = location.href = "sprintboard.html";
-        document.getElementById("container").appendChild(button);
-    }
+    import Button from '@/components/Button.vue';
+    import addTaskCardToProductBacklog from '@/components/addCardToProductBacklog.vue';
 
-    function newSprintPage() {
-        var opened = window.open("");
-        opened.document.write(
-            "<html><head><title>MyTitle</title></head><body>test</body></html>"
-        );
-    }
+    export default{
+        props: {
+            cards: Array
+        },
 
-    // -------------------------------------------------------------------------------------
+        mounted(){
+            this.displayCards = this.cards
+        },
 
-    const list_items = document.querySelectorAll(".list-item");
-    const lists = document.querySelectorAll(".list");
+        components: {
+            Button,
+        },
 
-    let draggedItem = null;
+        methods: {
+            displayProductBacklog() {
+                this.$emit("add-card",card)
+                this.displayCards=this.cards
+            }
+        },
 
-    for (let i = 0; i < list_items.length; i++) {
-        const item = list_items[i];
-
-        item.addEventListener("dragstart", function () {
-            draggedItem = item;
-            setTimeout(function () {
-                item.style.display = "none";
-            }, 0);
-        });
-
-        item.addEventListener("dragend", function () {
-            setTimeout(function () {
-                draggedItem.style.display = "block";
-                draggedItem = null;
-            }, 0);
-        });
-
-        for (let j = 0; j < lists.length; j++) {
-            const list = lists[j];
-
-            list.addEventListener("dragover", function (e) {
-                e.preventDefault();
-            });
-
-            list.addEventListener("dragenter", function (e) {
-                e.preventDefault();
-                this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
-            });
-
-            list.addEventListener("dragleave", function (e) {
-                this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-            });
-
-            list.addEventListener("drop", function (e) {
-                console.log("drop");
-                this.append(draggedItem);
-                this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-            });
+        data() {
+            return {
+                displayCards: [],
+            }
         }
     }
 
