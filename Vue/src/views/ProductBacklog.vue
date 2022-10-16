186f3f73416a221119bf20f4ad2eb0c2859b36eb
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-10-16 15:44:15 +0800

    Added a method to add time to cards in sprint backlog

--- Vue/src/views/ProductBacklog.vue
+++ Vue/src/views/ProductBacklog.vue
@@ -31,12 +31,13 @@
                         </div>
                         <div>Assigned To: {{card.assign}}</div>
                         <span class="tag" v-for="tag in card.tags">{{tag}}</span>
+                        <div>Time: {{card.time}}</div>
                     </div>  
                 </div>
             </div>        
         </div>
     
-        <div class="modal fade" id="cardPopupProductBacklog" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="cardPopupSprintBacklog" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                 <div class="modal-header">
@@ -61,104 +62,27 @@
             </div>
         </div>
 
-        <div class="modal fade" id="cardPopupInSprintBacklog" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">{{this.selectedCard.taskName}}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Description: {{this.selectedCard.description}}</p>
-                    <div>Status: {{this.selectedCard.status}}</div>
-                    <div>Type: {{this.selectedCard.type}}</div>
-                    <div class="story-points">
-                        {{this.selectedCard.storyPoints}}
-                    </div>
-                    <div>Assigned To: {{this.selectedCard.assign}}</div>
-                    <span class="tag" v-for="tag in this.selectedCard.tags">{{tag}}</span>
-                </div>
-                </div>
-            </div>
-        </div>
-
         <div class="modal fade" id="logTimeAndDate" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Task Details</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Log Time and Date</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                 <form id="myForm">
                     <div class="mb-3">
-                    <label for="task-title" class="col-form-label">Task Title:</label>
-                    <input type="text" class="form-control" id="edit-title" placeholder="Enter task title here" v-model="editTitle" />
-                    </div>
-                    <div class="mb-3">
-                    <label for="task-desc" class="col-form-label">Task Description:</label>
-                    <textarea class="form-control" id="edit-desc" placeholder="Enter task description here"></textarea>
-                    </div>
-                    <div class="mb-3">
-                    <label for="task-status" class="col-form-label">Status:</label>
-                    <br />
-                    <select class="status" id="edit-status">
-                        <option value="To Do/Blocked">To Do/Blocked</option>
-                        <option value="In Progress">In Progress</option>
-                        <option value="Deployed">Deployed</option>
-                        <option value="Done">Done</option>
-                    </select>
-                    </div>
-                    <div class="mb-3">
-                    <label for="story-points" class="col-form-label">Story Points (between 1 and 10):</label>
-                    <input type="number" class="form-control" id="edit-story-point" placeholder="Enter story points here" min="1" max="10">
-                    </div>
-                    <div class="mb-3">
-                    <label for="task-tags" class="col-form-label">Task Tag:</label>
-                    <br />
-                    <select class="tag" id="edit-tag">
-                        <option value="UI">UI</option>
-                        <option value="CORE">Core</option>
-                        <option value="Testing">Testing</option>
-                    </select>
-                    </div>
-                    <div class="mb-3">
-                    <label for="task-type" class="col-form-label">Task Type:</label>
-                    <br />
-                    <select class="type" id="edit-type">
-                        <option value="Bug">Bug</option>
-                        <option value="Story">Story</option>
-                    </select>
-                    </div>
-                    <div class="color">
-                    <label for="task-priority" class="col-form-label">Task Priority:</label>
-                    <br />
-                    <select class="priority" id="edit-priority" >
-                        <option value="critical">Critical</option>
-                        <option value="high">High</option>
-                        <option value="medium">Medium</option>
-                        <option value="low">Low</option>
-                    </select>
-                    <div class="mb-3">
-                    <label for="task-assign" class="col-form-label">Assigned to:</label>
-                    <br />
-                    <select class="assign" id="edit-assign">
-                        <option value="Dion">Dion</option>
-                        <option value="Hannah">Hannah</option>
-                        <option value="Lohan">Lohan</option>
-                        <option value="Taha">Taha</option>
-                        <option value="Rameez">Rameez</option>
-                    </select>
-                    </div>
-                    </div>
+                    <label for="task-title" class="col-form-label">Enter time:</label>
+                    <input type="text" class="form-control" id="accumulated-time" placeholder="Enter hours here" v-model="accumulatedTime" />
+                    </div>                
                 </form>
                 </div>
                 <div class="modal-footer">
                 <button type="button" class="btn btn-secondary btn-danger" data-bs-dismiss="modal">
-                    Close
+                    Cancel
                 </button>
-                <button type="button" class="btn btn-primary" data-toggle="modal" id="button-save" data-bs-dismiss="modal" @click="editTaskCard(this.onClickCardID)">
-                    Confirm Changes
+                <button type="button" class="btn btn-primary" data-toggle="modal" id="button-save" data-bs-dismiss="modal" @click="addTimeToCard(this.onClickCardID)">
+                    Add Time
                 </button>
                 </div>
             </div>
@@ -238,7 +162,18 @@
                 this.selectedCard = this.sprintBacklog.find((card)=>card.id ===id)
                 console.log(this.sprintBacklog)
                 console.log(this.sprintBacklog.find((card)=>card.id ===id))
-                // this.$emit("edit-card",id)
+                // this.$emit("edit-card",id)        
+                },
+            
+            addTimeToCard(id) {
+                const index = this.sprintBacklog.findIndex((task)=>task.id ===id)
+                let newTime = document.getElementById('accumulated-time')
+                console.log(newTime)
+
+                card.time = newTime
+                console.log(card)
+                this.$emit("time",index,card)
+                this.sprintBacklog[index] = card
             },
 
             startDrag(event, card) {
