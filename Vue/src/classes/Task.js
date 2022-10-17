8ef78567da65c1c3da0bdae5ce19d8091f31bce7
lohan <lvid0003@student.monash.edu>
2022-10-17 11:58:43 +0800

    Reformatted Code

--- Vue/src/classes/Task.js
+++ Vue/src/classes/Task.js
@@ -2,10 +2,17 @@ export default class Task {
     constructor(taskList = []) {
         this._taskList = taskList;
     }
+
     //-------ACCESSORS---------------
-    get taskList() { return this._taskList };
+    get taskList() {
+        return this._taskList
+    }
+
     //-------MUTATORS-----------------
-    set taskList(newTaskList) { this._taskList = newTaskList };
+    set taskList(newTaskList) {
+        this._taskList = newTaskList
+    }
+
     //-------METHOD---------------
     fromData(data) {
         this._taskList = data._taskList;
