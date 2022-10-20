ec3333d963c485861310cd936af4a916c48e505d
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-10-20 11:58:20 +0800

    moved the popup for log time and date to sprint backlog and removed the click function on cards

--- Vue/src/views/ProductBacklog.vue
+++ Vue/src/views/ProductBacklog.vue
@@ -48,10 +48,12 @@
       </li>
     </ul>
   </nav>
+
   <div class="container text-start">
     <div class="row align-items-top">
       <div class="col" @drop="onDrop($event, 'product')" @dragover.prevent @dragenter.prevent>
-        <div @click="onClickCardInProductBacklog(card.id)" class="card row" :class="card.priority"
+        <h3 id="col-header1">Product Backlog</h3>
+        <div class="card row" :class="card.priority"
              data-bs-toggle="modal" data-bs-target="#cardPopupProductBacklog" v-for="card in this.productBacklog"
              draggable="true" @dragstart="startDrag($event, card)">
           <h3>{{ card.taskName }}</h3>
@@ -66,7 +68,8 @@
         </div>
       </div>
       <div class="col" @drop="onDrop($event, 'sprint')" @dragover.prevent @dragenter.prevent>
-        <div @click="onClickCardInSprintBacklog(card.id)" class="card row" :class="card.priority" data-bs-toggle="modal"
+        <h3 id="col-header2">Sprint Backlog</h3>
+        <div class="card row" :class="card.priority" data-bs-toggle="modal"
              data-bs-target="#cardPopupSprintBacklog" v-for="card in this.sprintBacklog"
              draggable="true" @dragstart="startDrag($event, card)">
           <h3>{{ card.taskName }}</h3>
@@ -78,140 +81,6 @@
           </div>
           <div>Assigned To: {{ card.assign }}</div>
           <span class="tag" v-for="tag in card.tags">{{ tag }}</span>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="modal fade" id="cardPopupProductBacklog" tabindex="-1" aria-labelledby="exampleModalLabel"
-       aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">{{ this.selectedCard.taskName }}</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <p>Description: {{ this.selectedCard.description }}</p>
-          <div>Status: {{ this.selectedCard.status }}</div>
-          <div>Type: {{ this.selectedCard.type }}</div>
-          <div class="story-points">
-            {{ this.selectedCard.storyPoints }}
-          </div>
-          <div>Assigned To: {{ this.selectedCard.assign }}</div>
-          <span class="tag" v-for="tag in this.selectedCard.tags">{{ tag }}</span>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                  data-bs-target="#popUpForLogWork">Log Time and Date
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="modal fade" id="cardPopupInSprintBacklog" tabindex="-1" aria-labelledby="exampleModalLabel"
-       aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">{{ this.selectedCard.taskName }}</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <p>Description: {{ this.selectedCard.description }}</p>
-          <div>Status: {{ this.selectedCard.status }}</div>
-          <div>Type: {{ this.selectedCard.type }}</div>
-          <div class="story-points">
-            {{ this.selectedCard.storyPoints }}
-          </div>
-          <div>Assigned To: {{ this.selectedCard.assign }}</div>
-          <span class="tag" v-for="tag in this.selectedCard.tags">{{ tag }}</span>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="modal fade" id="logTimeAndDate" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Task Details</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <form id="myForm">
-            <div class="mb-3">
-              <label for="task-title" class="col-form-label">Task Title:</label>
-              <input type="text" class="form-control" id="edit-title" placeholder="Enter task title here"/>
-            </div>
-            <div class="mb-3">
-              <label for="task-desc" class="col-form-label">Task Description:</label>
-              <textarea class="form-control" id="edit-desc" placeholder="Enter task description here"></textarea>
-            </div>
-            <div class="mb-3">
-              <label for="task-status" class="col-form-label">Status:</label>
-              <br/>
-              <select class="status" id="edit-status">
-                <option value="To Do/Blocked">To Do/Blocked</option>
-                <option value="In Progress">In Progress</option>
-                <option value="Deployed">Deployed</option>
-                <option value="Done">Done</option>
-              </select>
-            </div>
-            <div class="mb-3">
-              <label for="story-points" class="col-form-label">Story Points (between 1 and 10):</label>
-              <input type="number" class="form-control" id="edit-story-point" placeholder="Enter story points here"
-                     min="1" max="10">
-            </div>
-            <div class="mb-3">
-              <label for="task-tags" class="col-form-label">Task Tag:</label>
-              <br/>
-              <select class="tag" id="edit-tag">
-                <option value="UI">UI</option>
-                <option value="CORE">Core</option>
-                <option value="Testing">Testing</option>
-              </select>
-            </div>
-            <div class="mb-3">
-              <label for="task-type" class="col-form-label">Task Type:</label>
-              <br/>
-              <select class="type" id="edit-type">
-                <option value="Bug">Bug</option>
-                <option value="Story">Story</option>
-              </select>
-            </div>
-            <div class="color">
-              <label for="task-priority" class="col-form-label">Task Priority:</label>
-              <br/>
-              <select class="priority" id="edit-priority">
-                <option value="critical">Critical</option>
-                <option value="high">High</option>
-                <option value="medium">Medium</option>
-                <option value="low">Low</option>
-              </select>
-              <div class="mb-3">
-                <label for="task-assign" class="col-form-label">Assigned to:</label>
-                <br/>
-                <select class="assign" id="edit-assign">
-                  <option value="Dion">Dion</option>
-                  <option value="Hannah">Hannah</option>
-                  <option value="Lohan">Lohan</option>
-                  <option value="Taha">Taha</option>
-                  <option value="Rameez">Rameez</option>
-                </select>
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary btn-danger" data-bs-dismiss="modal">
-            Close
-          </button>
-          <button type="button" class="btn btn-primary" data-toggle="modal" id="button-save" data-bs-dismiss="modal"
-                  @click="editTaskCard(this.onClickCardID)">
-            Confirm Changes
-          </button>
         </div>
       </div>
     </div>
@@ -286,7 +155,7 @@
             Close
           </button>
           <button type="button" class="btn btn-primary" data-toggle="modal" id="button-save" data-bs-dismiss="modal"
-                  @click="">
+                  @click="logTimeAndDateForCard(this.onClickCardID)">
             Confirm
           </button>
         </div>
@@ -330,13 +199,17 @@ export default {
     console.log('mount')
     console.log(this.$route.query.clickSprint)
 
-    this.sprintID = this.$route.query.id
-    if (this.$route.query.id) {
-      this.sprintID = parseInt(this.sprintID)
-    } else {
-      this.sprintID = -1
-    }
-
+    console.log("before",this.sprintID)
+    console.log("befpre",this.$route.query.id)
+    // this.sprintID = this.$route.query.id
+    console.log("after",this.sprintID)
+    console.log("After",this.$route.query.id)
+    // if (this.$route.query.id) {
+    //   this.sprintID = parseInt(this.sprintID)
+    // } else {
+    //   this.sprintID = -1
+    // }
+    // this.sprintID = (this.sprintID)
     console.log(parseInt(this.sprintID))
 
     console.log("id", this.sprintID)
@@ -380,15 +253,6 @@ export default {
       this.$emit("add-card", card)
     },
 
-    onClickCardInSprintBacklog(id) {
-      this.cardId = id
-      // this.showCardInSprintBacklog = !this.showCardInSprintBacklog
-      this.selectedCard = this.sprintBacklog.find((card) => card.id === id)
-      console.log(this.sprintBacklog)
-      console.log(this.sprintBacklog.find((card) => card.id === id))
-      // this.$emit("edit-card",id)
-    },
-
     startDrag(event, card) {
       // console.log(card)
       event.dataTransfer.dropEffect = 'move'
@@ -414,7 +278,7 @@ export default {
 
       }
       localStorage.setItem("cards", JSON.stringify(this.productBacklog))
-      localStorage.setItem("sprint" + this.sprintID, JSON.stringify(this.sprintBacklog))
+      localStorage.setItem("sprint" + parseInt(this.sprintID), JSON.stringify(this.sprintBacklog))
 
     },
     addToSprintBoard() {
@@ -425,17 +289,17 @@ export default {
         startDate: this.sprintStartDate,
         endDate: this.sprintEndDate
       }
+      
       // console.log(sprint)
       this.$emit('add-to-sprint', sprint)
       this.$router.back()
       // this.$router.push('sprintboard')
-    }
+    },
   },
 
   data() {
     return {
       productBacklog: [],
-      showCardInProductBacklog: false,
       selectedCard: {},
       sprintBacklog: [],
       onCreate: true,
@@ -445,8 +309,6 @@ export default {
       logTimeStartDate: '',
       logTimeEndDate: '',
       sprintID: 0,
-
-
     }
   }
 }
@@ -491,6 +353,7 @@ export default {
   text-align: center;
   margin: 4px 0;
 }
+
 .card {
   background: white;
   box-shadow: 0 0 30px rgba(0, 0, 0, 0.15);
@@ -531,18 +394,46 @@ export default {
       margin-bottom: 3px;
     }
   }
-  .h3{
+
+  .h3 {
     color: blue;
   }
+
   .p {
     margin-bottom: 0;
   }
 }
+
 #sprintStartDate, #sprintEndDate {
   padding-left: 5px;
   margin-left: 5px;
 }
-#timeLog{
+
+#timeLog {
   margin-left: 5px;
+}
+
+div.col {
+  box-shadow: 0 0 0 5px #9d3b3b;
+  border-radius: 1em;
+  padding: 1em 2em;
+  margin-top: 15px;
+}
+
+#col1 {
+  margin-right: 20px;
+}
+
+#col2 {
+  margin-left: 20px;
+}
+
+#col-header1, #col-header2 {
+  color: white;
+  text-align: center;
+  text-transform: uppercase;
+  font-family: "Century Gothic", AppleGothic, sans-serif;
+  font-weight: bold;
+  background: -webkit-linear-gradient(#e16dbb, #ef8983);
 }
 </style>
